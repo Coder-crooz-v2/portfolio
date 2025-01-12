9f21f04b664c5adb539bff2a7eb7dc42247613c7
Coder-crooz-v2 <chaitalibhawal801@gmail.com>
2025-01-13 01:15:14 +0530

    bug fixes and addition of features

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,15 +1,45 @@
 import { Canvas } from "@react-three/fiber";
 import { Decal, Float, OrbitControls, useTexture } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
+import { Suspense } from "react";
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
 const Shape = ({ content }: { content: { icon: string } }) => {
     const [texture] = useTexture([content.icon]);
 
     return (
             <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-                <ambientLight intensity={0.25} />
+                <ambientLight intensity={0.5} />
                 <directionalLight position={[0, 0, 0.05]} />
                 <mesh castShadow receiveShadow scale={1.75}>
-                    <icosahedronGeometry args={[1, 1]} />
+                    <icosahedronGeometry args={[1.5, 2]} />
                     <meshStandardMaterial color="#b4b0ff" polygonOffset polygonOffsetFactor={-5} flatShading />
                     <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={texture} />
                 </mesh>
@@ -19,7 +49,11 @@ const Shape = ({ content }: { content: { icon: string } }) => {
 
 const Skills = ({ skill }: { skill: { icon: string } }) => {
     return (
-        <Canvas frameloop="demand">
+        <Canvas 
+        frameloop='demand'
+        dpr={[1, 2]}
+        gl={{ preserveDrawingBuffer: true }}>
+            <Suspense fallback={<CanvasLoader />}/>
             <OrbitControls enableZoom={false} enablePan={false} />
             <Shape content = {skill}/>
         </Canvas>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,11 @@
 import react from "../assets/react.png";
 import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import git from "../assets/git.png";
+import express from "../assets/express.png";
+import node from "../assets/node.png";
+import mongodb from "../assets/mongodb.png";
 
 export const skills = [
     {
@@ -7,7 +13,31 @@ export const skills = [
         icon: html,
     },
     {
+        name: "CSS",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
         name: "React",
         icon: react,
-    }
+    },
+    {
+        name: "Git",
+        icon: git,
+    },
+    {
+        name: "NodeJs",
+        icon: node,
+    },
+    {
+        name: "Express.js",
+        icon: express,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
 ]
