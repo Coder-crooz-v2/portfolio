8cdd8046b7331950dcb574b281f3c001d8ba6f74
Coder-crooz-v2 <chaitalibhawal801@gmail.com>
2025-01-15 03:21:05 +0530

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import AboutMe from './pages/AboutMe';
 import CodingStats from './pages/CodingStats';
-import Projects from './pages/Projects';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe/>} />
           <Route path="/codingstats" element={<CodingStats/>} />
-          <Route path="/projects" element={<Projects/>} />
           <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
